7821d65abaf8270f2fa18f3e3cace6f4b71e3dfe
https://github.com/Ungarov7 <example@gmail.com>
2025-02-05 19:21:35 +0600

    Layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../../lib/auth-options";
 import Auth from "../../components/auth";
+import { Toaster } from "../../components/ui/toaster";
+import NextTopLoader from "nextjs-toploader";
+import Sidebar from "../../components/sidebar/sidebar";
+import FollowBar from "../../components/shared/follow-bar";
 
 
 interface Props {
@@ -18,7 +22,31 @@ const Layout = async ({ children }: Props) => {
         );
     }
 
-    return <div>{children}</div>
+    return (
+        <div className="lg:container h-screen mx-auto lg:max-w-7xl">
+            <div className="flex">
+                <Sidebar user={JSON.parse(JSON.stringify(session.currentUser))}/>
+                <div className="flex flex-1 border-x-[10px] border-neutral-800 lg:mx-4 ml-1">
+                    <div className="w-full">
+                        <NextTopLoader
+                        color="#2299DD"
+                        initialPosition={0.08}
+                        crawlSpeed={200}
+                        height={3}
+                        crawl={true}
+                        showSpinner={false}
+                        easing="ease"
+                        speed={200}
+                        shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+                        />
+                        {children}
+                        <Toaster/>
+                    </div>
+                </div>
+                <FollowBar/>
+            </div>
+        </div>
+    )
 };
 
 export default Layout;
