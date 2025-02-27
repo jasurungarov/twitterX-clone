939244930fe44bb45f087744b95b0dd7dad5ba0a
jasurungarov <muhammadaliungarov@gmail.com>
2025-02-27 18:16:29 +0600

    Post detail

--- components/shared/post-item.tsx
+++ components/shared/post-item.tsx
@@ -10,6 +10,7 @@ import { FaHeart } from "react-icons/fa";
 import { toast } from "../ui/use-toast";
 import axios from "axios";
 import { Loader2 } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface Props {
   post: IPost;
@@ -20,7 +21,10 @@ interface Props {
 const PostItem = ({ post, user, setPosts }: Props) => {
   const [isLoading, setIsLoading] = useState(false);
 
-  const onDelete = async () => {
+  const router = useRouter();
+
+  const onDelete = async (e: any) => {
+    e.stopPropagation();
     try {
       setIsLoading(true);
       await axios.delete(`/api/posts`, {
@@ -40,6 +44,60 @@ const PostItem = ({ post, user, setPosts }: Props) => {
     }
   };
 
+  const onLike = async (e: any) => {
+    e.stopPropagation();
+    try {
+      setIsLoading(true);
+      if (post.hasLiked) {
+        await axios.delete(`/api/likes`, {
+          data: {
+            postId: post._id,
+            userId: user._id,
+          },
+        });
+
+        const updatedPosts = {
+          ...post,
+          hasLiked: false,
+          likes: post.likes - 1,
+        };
+
+        setPosts((prev) =>
+          prev.map((p) => (p._id === post._id ? updatedPosts : p))
+        );
+      } else {
+        await axios.put(`/api/likes`, {
+          postId: post._id,
+          userId: user._id,
+        });
+
+        const updatedPosts = {
+          ...post,
+          hasLiked: true,
+          likes: post.likes + 1,
+        };
+
+        setPosts((prev) =>
+          prev.map((p) => (p._id === post._id ? updatedPosts : p))
+        );
+      }
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      return toast({
+        title: "Error",
+        description: "Something went wrong. Please try again later.",
+        variant: "destructive",
+      });
+    }
+  };
+
+  const goToPost = () => {
+    router.push(`/posts/${post._id}`);
+  };
+
+
   return (
     <div className="border-b-[1px] border-neutral-800 p-5 cursor-pointer hover:bg-neutral-900 transition relative">
       {isLoading && (
@@ -49,7 +107,7 @@ const PostItem = ({ post, user, setPosts }: Props) => {
           </div>
         </div>
       )}
-      <div className="flex flex-row items-center gap-3">
+      <div className="flex flex-row items-center gap-3 cursor-pointer" onClick={goToPost}>
         <Avatar>
           <AvatarImage src={post.user.profileImage} />
           <AvatarFallback>{post.user.name[0]}</AvatarFallback>
@@ -66,7 +124,7 @@ const PostItem = ({ post, user, setPosts }: Props) => {
                 : sliceText(post.user.email, 16)}
             </span>
             <span className="text-neutral-500 text-sm">
-              {/* {formatDistanceToNowStrict(new Date(post.createdAt))} ago */}
+              {/* {formatDistanceToNowStrict(new Date(post.createdAt))} ago */} ago
             </span>
           </div>
 
@@ -79,10 +137,10 @@ const PostItem = ({ post, user, setPosts }: Props) => {
             </div>
 
             <div
-              className={`flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500`}
+              className={`flex flex-row items-center text-neutral-500 gap-2 cursor-pointer transition hover:text-red-500`} onClick={onLike}
             >
-              <FaHeart size={20} color={"red"} />
-              <p>{post.likes.length}</p>
+              <FaHeart size={20} color={post.hasLiked ? "red" : ""} />
+              <p>{post.likes.length || 0}</p>
             </div>
 
             {post.user._id === user._id && (
